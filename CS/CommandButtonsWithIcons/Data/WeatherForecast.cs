cb29c80b6c16bee81303dfade447ece21a9d1f88
Elena Khamlyuk <elena.khamlyuk@devexpress.com>
2022-06-15 12:21:19 +0300

    upd

--- CS/CommandButtonsWithIcons/Data/WeatherForecast.cs
+++ CS/CommandButtonsWithIcons/Data/WeatherForecast.cs
@@ -2,12 +2,10 @@ namespace CommandButtonsWithIcons.Data
 {
     public class WeatherForecast
     {
+        public int ID { get; set; }
         public DateTime Date { get; set; }
-
         public int TemperatureC { get; set; }
-
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
         public string? Summary { get; set; }
     }
 }
--- CS/CommandButtonsWithIcons/Data/WeatherForecastService.cs
+++ CS/CommandButtonsWithIcons/Data/WeatherForecastService.cs
@@ -1,67 +1,36 @@
 namespace CommandButtonsWithIcons.Data {
     public class WeatherForecastService {
-        public string[] Summaries = new[]
-        {
-            "Hot", "Warm", "Cold", "Freezing"
+        public readonly string[] Summaries = new[] {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
-
-        private List<WeatherForecast> CreateForecast() {
-            var rng = new Random();
-            DateTime startDate = DateTime.Now;
-            return Enumerable.Range(1, 15).Select(index => new WeatherForecast {
-                Date = startDate.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            }).ToList();
-        }
-
-        public List<WeatherForecast> Forecasts { get; set; }
-        public WeatherForecastService() {
-            Forecasts = CreateForecast();
-        }
-        public Task<IEnumerable<WeatherForecast>> GetForecastAsync(CancellationToken ct = default) {
-            return Task.FromResult<IEnumerable<WeatherForecast>>(Forecasts);
-        }
-        public Task<string[]> GetSummariesAsync() {
-            return Task.FromResult(Summaries);
-        }
-        void InsertInternal(IDictionary<string, object> newValue) {
-            var dataItem = new WeatherForecast();
-            Update(dataItem, newValue);
-            Forecasts.Insert(0, dataItem);
-        }
-        public Task Insert(IDictionary<string, object> newValue) {
-            InsertInternal(newValue);
-            return Task.CompletedTask;
-        }
-        void RemoveInternal(WeatherForecast dataItem) {
-            Forecasts.Remove(dataItem);
-        }
-        public Task Remove(WeatherForecast dataItem) {
-            RemoveInternal(dataItem);
-            return Task.CompletedTask;
-        }
-        void UpdateInternal(WeatherForecast dataItem, IDictionary<string, object> newValue) {
-            foreach (var field in newValue.Keys) {
-                switch (field) {
-                    case "Date":
-                        dataItem.Date = (DateTime)newValue[field];
-                        break;
-                    case "Summary":
-                        dataItem.Summary = (string)newValue[field];
-                        break;
-                    case "TemperatureC":
-                        int intValue = 0;
-                        if (Int32.TryParse((string)newValue[field], out intValue)) {
-                            dataItem.TemperatureC = intValue;
-                        }
-                        break;
-                }
+        private List<WeatherForecast>? Forecasts;
+        public Task<List<WeatherForecast>> GetForecastAsync() {
+            if (Forecasts == null) {
+                var rng = new Random();
+                Forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast {
+                    ID = index,
+                    Date = DateTime.Today.AddDays(index),
+                    TemperatureC = rng.Next(-20, 55),
+                    Summary = Summaries[rng.Next(Summaries.Length)]
+                }).ToList();
             }
+            return Task.FromResult(Forecasts);
         }
-        public Task Update(WeatherForecast dataItem, IDictionary<string, object> newValue) {
-            UpdateInternal(dataItem, newValue);
-            return Task.CompletedTask;
+        public Task<List<WeatherForecast>> ChangeForecastAsync(WeatherForecast changed) {
+            var orginalForecast = Forecasts.FirstOrDefault(i => i.ID == changed.ID);
+            if (orginalForecast != null) {
+                orginalForecast.TemperatureC = changed.TemperatureC;
+                orginalForecast.Summary = changed.Summary;
+                orginalForecast.Date = changed.Date;
+            }
+            else {
+                Forecasts.Add(changed);
+            }
+            return Task.FromResult(Forecasts);
+        }
+        public Task<List<WeatherForecast>> Remove(WeatherForecast forecast) {
+            Forecasts.Remove(forecast);
+            return Task.FromResult(Forecasts);
         }
     }
 }
