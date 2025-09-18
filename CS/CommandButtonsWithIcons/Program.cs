27610341d444040cf5be0e9a11269205ff04cdee
Elena Peskova <116714417+elenapeskova@users.noreply.github.com>
2025-09-18 12:21:52 +0400

    Refactor Program.cs for DevExpress configuration

--- CS/CommandButtonsWithIcons/Program.cs
+++ CS/CommandButtonsWithIcons/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddServerSideBlazor();
 builder.Services.AddDevExpressBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.Configure<DevExpress.Blazor.Configuration.GlobalOptions>(options => {
-    options.BootstrapVersion = DevExpress.Blazor.BootstrapVersion.v5;
+    options.SizeMode = DevExpress.Blazor.SizeMode.Medium;
 });
 builder.WebHost.UseWebRoot("wwwroot");
 builder.WebHost.UseStaticWebAssets();
@@ -34,5 +34,6 @@ app.UseRouting();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+
 
 app.Run();
